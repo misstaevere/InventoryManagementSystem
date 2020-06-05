2c0a258de74b10427f6189d43472819f5b2278db
Kart Taevere <kartaevere@gmail.com>
2020-06-05 13:38:24 +0100

    data.sql changed foreign keys

--- src/test/resources/ims-project-data.sql
+++ src/test/resources/ims-project-data.sql
@@ -1,7 +1,5 @@
 use testdb;
-INSERT INTO item (item_name, qty_ordered, item_price, item_category) VALUES ('Floral Maxi Dress', 1, 95.00, 'Dresses');
-INSERT INTO item (item_name, qty_ordered, item_price, item_category) VALUES ('Chunky Sandals in Black', 1, 50.00, 'Shoes');
-INSERT INTO item (item_name, qty_ordered, item_price, item_category)VALUES ('2-pack Face Mask in Bandana Print', 5, 12.00, 'Accessories');
+INSERT INTO item (item_name,  item_price, item_category) VALUES ('Floral Maxi Dress', 95.00), ('Chunky Sandals in Black', 50.00), ('2-pack Face Mask in Bandana Print', 12.00);
 UPDATE item SET item_price=45 WHERE item_name='Chunky Sandals in Black';
 INSERT INTO customer (customer_first_name, customer_last_name, customer_email, customer_password) VALUES ('Simon', 'Smith', 'si@mail.co.uk', '123456');
 INSERT INTO customer (customer_first_name, customer_last_name, customer_email, customer_password) VALUES ('Maria', 'Morris', 'mm@gmail.com', 'qwerty');
@@ -12,6 +10,6 @@ INSERT INTO my_order (fk_customer_ID, my_order_placed) VALUES (2, '2020-04-03');
 INSERT INTO my_order (fk_customer_ID, my_order_placed) VALUES (3, '2020-05-03');
 UPDATE my_order SET my_order_ID = 100 WHERE my_order_placed = '2020-03-03';
 INSERT INTO orderline (fk_my_order_ID, fk_item_ID) VALUES (1, 1);
-INSERT INTO orderline (fk_my_order_ID, fk_item_ID) VALUES (2, 5);
-INSERT INTO orderline (fk_my_order_ID, fk_item_ID) VALUES (2, 8);
+INSERT INTO orderline (fk_my_order_ID, fk_item_ID) VALUES (2, 2);
+INSERT INTO orderline (fk_my_order_ID, fk_item_ID) VALUES (2, 3);
 UPDATE orderline SET qty_ordered = 107 WHERE orderline_ID = 1;
